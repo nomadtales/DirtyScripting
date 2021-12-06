1045d1e98fe409fa756e54b6f452947488c31de1
NomadTales <nomadtales@gmail.com>
2021-12-06 17:58:41 +1100

    add if

--- PowerShell/Suspend-RDR2.ps1
+++ PowerShell/Suspend-RDR2.ps1
@@ -2,8 +2,12 @@
 # Invoke-Command -ScriptBlock ([ScriptBlock]::Create((Invoke-WebRequest 'https://raw.githubusercontent.com/nomadtales/DirtyScripting/master/PowerShell/Suspend-RDR2.ps1').Content))
 
 $ps = (Get-Process -Name RDR2).Id
-& 'C:\Program Files\PSTools\pssuspend64.exe' -nobanner "$ps"
-"RDR2 Paused"
-Start-Sleep -Seconds 7
-& 'C:\Program Files\PSTools\pssuspend64.exe' -r -nobanner "$ps"
-"RDR2 Resumed"
+
+If ($ps)
+{
+    & 'C:\Program Files\PSTools\pssuspend64.exe' -nobanner "$ps"
+    "RDR2 Paused"
+    Start-Sleep -Seconds 7
+    & 'C:\Program Files\PSTools\pssuspend64.exe' -r -nobanner "$ps"
+    "RDR2 Resumed"
+}
