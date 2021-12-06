b1746f3f8db1264a3c223c636c68ff04ee4fff63
NomadTales <nomadtales@gmail.com>
2021-12-06 17:55:04 +1100

    tweaks

--- PowerShell/Suspend-RDR2.ps1
+++ PowerShell/Suspend-RDR2.ps1
@@ -1,6 +1,9 @@
+
+# Invoke-Command -ScriptBlock ([ScriptBlock]::Create((Invoke-WebRequest 'https://raw.githubusercontent.com/nomadtales/DirtyScripting/master/PowerShell/Suspend-RDR2.ps1').Content))
+
 $ps = (Get-Process -Name RDR2).Id
-& 'C:\Program Files\PSTools\pssuspend64.exe' "$ps"
+& 'C:\Program Files\PSTools\pssuspend64.exe' -nobanner "$ps"
 "RDR2 Paused"
 Start-Sleep -Seconds 7
-& 'C:\Program Files\PSTools\pssuspend64.exe' -r "$ps"
+& 'C:\Program Files\PSTools\pssuspend64.exe' -r -nobanner "$ps"
 "RDR2 Resumed"
