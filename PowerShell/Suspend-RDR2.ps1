92fc1277e615dd255c0bf190cd5f2f9b184896ce
NomadTales <nomadtales@gmail.com>
2021-12-06 18:09:13 +1100

    updates

--- PowerShell/Suspend-RDR2.ps1
+++ PowerShell/Suspend-RDR2.ps1
@@ -1,13 +1,18 @@
+# Requires PSTools to work. https://docs.microsoft.com/en-us/sysinternals/downloads/pstools
+# Make sure you put the PSTools into Path
+# Create a desktop shortcut using the below:
+# powershell.exe -command "Invoke-Command -ScriptBlock ([ScriptBlock]::Create((Invoke-WebRequest 'https://raw.githubusercontent.com/nomadtales/DirtyScripting/master/PowerShell/Suspend-RDR2.ps1').Content))"
 
-# Invoke-Command -ScriptBlock ([ScriptBlock]::Create((Invoke-WebRequest 'https://raw.githubusercontent.com/nomadtales/DirtyScripting/master/PowerShell/Suspend-RDR2.ps1').Content))
-
-$ps = (Get-Process -Name RDR2).Id
+$ps = (Get-Process -Name RDR2 -ErrorAction SilentlyContinue).Id
 
 If ($ps)
 {
-    & 'C:\Program Files\PSTools\pssuspend64.exe' -nobanner "$ps"
+    & 'pssuspend64.exe' -nobanner "$ps"
     "RDR2 Paused"
     Start-Sleep -Seconds 7
-    & 'C:\Program Files\PSTools\pssuspend64.exe' -r -nobanner "$ps"
+    & 'pssuspend64.exe' -r -nobanner "$ps"
     "RDR2 Resumed"
 }
+Else {"RDR2 Not Running"}
+
+Start-Sleep -Seconds 10
