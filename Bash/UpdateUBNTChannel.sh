6f818bb5b87789e61bd7ec3479509b5e39372693
Wob76 <quotidian@gmail.com>
2022-09-27 08:36:04 +1000

    Update UpdateUBNTChannel.sh
    Modified outputs to include desired frequency rather than "DFS", included notify lines for use in UNRAID system.

--- Bash/UpdateUBNTChannel.sh
+++ Bash/UpdateUBNTChannel.sh
@@ -10,7 +10,9 @@ if [ $output -eq 0 ]
 
 then
 
-echo $(date +"%Y%m%d-%H%M%S") "NOT ON DFS, SHOULD BE SWITCHING"
+echo $(date +"%Y%m%d-%H%M%S") "NOT ON $freq, Switch is needed"
+
+/usr/local/emhttp/webGui/scripts/notify -s "Dish Frequecy has changed" -i "alert" -m "Dish is not on the desired frequecy of $freq"  -d "Dish is not on the desired frequecy of $freq"
 
 outputu=$(ssh $user@$ip -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedKeyTypes=+ssh-rsa 'grep -n "Radar found on freq.*.'$freqc'" /var/log/messages | tail -1 | cut -f1 -d:')
 
@@ -24,17 +26,21 @@ if [ $outputa -ge $outputu ]
 
 then
 
-echo $(date +"%Y%m%d-%H%M%S") "DFS Channel available (again), SWITCHING"
+echo $(date +"%Y%m%d-%H%M%S") "$freq is available, SWITCHING"
 
 outputc=$(ssh $user@$ip -o HostKeyAlgorithms=+ssh-rsa -o PubkeyAcceptedKeyTypes=+ssh-rsa 'iwconfig ath0 freq '$freq)
 
 echo $outputc
 
+/usr/local/emhttp/webGui/scripts/notify -s "Dish Frequecy has changed" -i "normal" -m "Dish is being reset to frequecy: $freq"  -d "Dish is being reset to frequecy: $freq"
+
 exit 1
 
 else
 
-echo $(date +"%Y%m%d-%H%M%S") "DFS channel not (yet) available, NOT SWITCHING, staying on fallback channel)"
+echo $(date +"%Y%m%d-%H%M%S") "$freq is not available, NOT SWITCHING"
+
+/usr/local/emhttp/webGui/scripts/notify -s "Dish Frequecy has changed" -i "warning" -m "$freq is not free"  -d "Dish frequecy $freq is not available, will try again in 24 hours"
 
 exit 1
 
@@ -42,6 +48,7 @@ fi
 
 else
 
-echo $(date +"%Y%m%d-%H%M%S") "ON DFS"
+echo $(date +"%Y%m%d-%H%M%S") "ON $freq, No change required"
 
 fi
+echo
