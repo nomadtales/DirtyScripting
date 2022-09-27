fe78750e66fce7a2aaf1c6163ef84318045a7a27
NomadTales <nomadtales@gmail.com>
2022-09-27 10:53:15 +1000

    tweak

--- PowerShell/Get-HDTwitVids.ps1
+++ PowerShell/Get-HDTwitVids.ps1
@@ -1,5 +1,5 @@
 $ProgressPreference = 'Continue'
-$twimg = 'https://video.twimg.com/ext_tw_video/1451607491620573197/pu/pl/GBv9M31rgbSSXMlb.m3u8?tag=12&container=fmp4'
+$twimg = 'https://video.twimg.com/ext_tw_video/1574546540466692096/pu/pl/FXTCCfshpwMH7p1A.m3u8?variant_version=1&tag=12&container=fmp4'
 
 #$m3u8wrapper = Read-Host 'Enter .m3u8 url'
 
