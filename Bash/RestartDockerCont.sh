0b983fa339d8b74a000114b5baea5ec79db3fddc
Jeremy Dunlop <jeremy.dunlop@health.nsw.gov.au>
2024-03-13 10:00:46 +1100

    update

--- Bash/RestartDockerCont.sh
+++ Bash/RestartDockerCont.sh
@@ -2,4 +2,11 @@
 
 # run as sudo
 
-stopped= docker container list --filter "label=docker-volume-backup.stop-during-backup=true" --filter status=exited --quiet
+# capture the exited offen volume backup docker containers
+exited=(docker container list --filter "label=docker-volume-backup.stop-during-backup=true" --filter status=exited --quiet)
+
+# loop through the containers and start
+for i in "${exited[@]}"
+do
+    echo "$i"
+done
